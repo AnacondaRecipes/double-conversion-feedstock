141a885259f16e732050f11261f1a88ed5a97d84
TS-ANC <208963744+TS-ANC@users.noreply.github.com>
2025-06-23 13:08:08 +0200

    move test commands to run_test

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 set -ex
 
+if [[ ${target_platform} == unix-* ]]; then
+    test -f $PREFIX/lib/libdouble-conversion.a
+elif [[ ${target_platform} == linux-* ]]; then
+    test -f $PREFIX/lib/libdouble-conversion.so
+elif [[ ${target_platform} == osx-* ]]; then
+    test -f $PREFIX/lib/libdouble-conversion.dylib
+fi
+
 # Test shared library
 ${CXX} ${CXXFLAGS} -I${PREFIX}/include -c test.cc -o test.o
 ${CXX} ${LDFLAGS} -L${PREFIX}/lib test.o -ldouble-conversion -o test.exe
