79429ce73b407401b8144a724ca7f69e5d25c87b
TS-ANC <208963744+TS-ANC@users.noreply.github.com>
2026-02-11 16:48:26 +0100

    remove cross-linux

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,8 +4,6 @@ set -ex
 declare -a CMAKE_PLATFORM_FLAGS
 if [[ ${HOST} =~ .*darwin.* ]]; then
   CMAKE_PLATFORM_FLAGS+=(-DCMAKE_OSX_SYSROOT="${CONDA_BUILD_SYSROOT}")
-else
-  CMAKE_PLATFORM_FLAGS+=(-DCMAKE_TOOLCHAIN_FILE="${RECIPE_DIR}/cross-linux.cmake")
 fi
 
 # Build shared lib
